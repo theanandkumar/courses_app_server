efe734c44abf8a15e2397dadb788e145610b5071
theanandkumar <61023555+theanandkumar@users.noreply.github.com>
2024-01-28 20:30:55 +0530

    Update Controller.java

--- src/main/java/com/course/courses/controller/Controller.java
+++ src/main/java/com/course/courses/controller/Controller.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin
 @RestController
 public class Controller {
 
